b6e9785a9468151f94734352623243c0bc9352a3
NicolasCorvol <nicolas.corvol@gmail.com>
2024-10-04 14:06:02 +0200

    first steps

---
+++ include/variable.h
@@ -0,0 +1,17 @@
+#ifndef VARIABLE_H
+#define VARIABLE_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Variable
+{
+public:
+    string name;
+    vector<int> domain;
+
+    Variable(string name, vector<int> domain);
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#include "variable.h"
+#include "constraint.h"
+
 int main()
 {
-    std::cout << "Hello World " << std::endl;
+    Variable A = Variable("A", {1, 2, 3});
+    Variable B = Variable("B", {4, 2, 3});
+    std::vector<std::tuple<int, int>> tuples = {std::make_tuple(1, 2)};
+    Constraint cons = Constraint(A, B, tuples);
+
+    std::cout << A.name << std::endl;
+    std::cout << std::get<0>(cons.get_tuple(0)) << std::endl;
 }
